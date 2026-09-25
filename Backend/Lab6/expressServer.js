5d01c8ea5a3f38323607895cb4dc6f7c40032909
Akahat Vats <akshatavs1122@gmail.com>
2026-09-25 14:33:53 +0530

    Updated message

--- Backend/Lab6/expressServer.js
+++ Backend/Lab6/expressServer.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 
 let userData = [
